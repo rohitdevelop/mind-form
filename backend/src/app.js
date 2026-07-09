8272a48ce97c8cdaf56c8e86c86e7e7d47a74298
Rohit Singh <rohitdev124421@gmail.com>
2026-07-09 22:41:29 +0530

    routers done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
- const router = require("./routes/userRoutes");
+ const authRouter = require("./routes/userRoutes");
 
 const app = express();
  app.use(express.json());
- app.use('/api', router);
+ app.use('/api', authRouter);
  
 module.exports = app;
  
